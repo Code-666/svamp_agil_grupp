9bb8447ffbe6e50aa5d88121453c826f4ce1bd00
John Hillbom <69886111+Code-666@users.noreply.github.com>
2023-09-25 22:26:07 +0200

    test commit app.py

--- app.py
+++ app.py
@@ -7,19 +7,3 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, world and Svampar</p>"
 
-
-@app.route("/test")
-def test():
-    return "<p>Hello, Svamp. new endpoint.</p>"
-
-
-@app.route("/svamp")
-def svamp():
-    return "<p> Hello, Svampälskare. </p>"
-
-@app.route("/test2")
-def test2():
-    return '<p> Another test <p>'
-
-
-# my comment
