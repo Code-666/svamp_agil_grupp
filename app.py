48290b793cd268e392ab57909663833e61667448
Magnus Jensen <96913339+Allena101@users.noreply.github.com>
2023-09-25 11:53:33 +0200

    Add files via upload
    test if sync with pythonAnywhere works

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, world and Svampar</p>"
+
+
+@app.route("/test")
+def test():
+    return "<p>Hello, Svamp. new endpoint.</p>"
+
+
+@app.route("/svamp")
+def svamp():
+    return "<p> Hello, Svampälskare. </p>"
+
+
+# my comment
