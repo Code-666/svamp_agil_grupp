0f6683b25e3157101eddc03e4d8bccc9e81eeaaf
Allena101 <96913339+Allena101@users.noreply.github.com>
2023-09-26 21:23:37 +0200

    test branch

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
 
@@ -16,3 +17,15 @@ def test():
 @app.route("/svamp")
 def svamp():
     return "<p> Hello, Svampälskare. </p>"
+
+
+@app.route("/mj_test")
+def mj_test():
+    """
+    ...
+    """
+    return render_template("mj_test.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
