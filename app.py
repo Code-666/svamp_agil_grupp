9c67f0b7e9122b87325617bbe3a4ba2dc5a4951c
SebastianS <eldsebbe@gmail.com>
2023-10-04 10:47:04 +0200

    SebbeTest

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template, request, flash, redirect, url_for
+import pandas as pd
+import csv
 
 app = Flask(__name__)
 
@@ -33,6 +35,23 @@ def mainpage():
     ...
     """
     return render_template("mainpage.html")
+
+    #Test av inlag av svampar
+@app.route('/add-choice', methods=['POST'])
+def add_choice():
+    name = request.form['svamp']
+    poison = request.form['giftig']
+    img_name = request.form['img_nanm']
+    traits = request.form['filter']
+    new_row = {'Name': name, 'Poison': poison, 'Img_name': img_name, 'Traits': traits}
+    data = pd.read_csv("Svampar.csv")
+    data = data.append(new_row, ignore_index=True)
+    data.to_csv('Svampar.csv', index=False)
+
+    return render_template('Page1.html', tables=[data.to_html()], titles=[''])
+
+
+
 
 
 if __name__ == "__main__":
