3e17bd1cb4ecca5533be9c0436fbb5069b0ffbaa
HP <john.hillbom@gmail.com>
2023-10-18 20:33:16 +0200

    key best practice

--- chatbot.py
+++ chatbot.py
@@ -1,22 +1,22 @@
 import openai
 
-openai.api_key = "key"
+openai.api_key = "secret_key"
 
-def chat_with_gpt(prompt):
+def chat_with_gpt(prompt, MaxToken=50, outputs=3):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[{"role":"user","content":prompt}]
+        messages=[{"role":"user","content":prompt}],
+        max_tokens=MaxToken,
+        n = outputs,
     )
 
-    return response.choices[0].messages.content.strip()
+    return response.choices[0].message.content.strip()
 
-"""
 if __name__ == "__main__":
     while True:
         user_input = input("You: ")
         if user_input.lower() in ["quit", "exit", "bye"]:
             break
     
-    response = chat_with_gpt(user_input)
-    print("Chatbot: ", response)
-"""
+        response = chat_with_gpt(user_input)
+        print("Chatbot: ", response)
